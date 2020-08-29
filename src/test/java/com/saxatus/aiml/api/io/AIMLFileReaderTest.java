23a8434a7efa6a1086636e34f4d0f9174c004aaf
sglass <s.glass@intershop.de>
2020-08-29 23:04:46 +0200

    feat: reactivate test

--- src/test/java/com/saxatus/aiml/api/io/AIMLFileReaderTest.java
+++ src/test/java/com/saxatus/aiml/api/io/AIMLFileReaderTest.java
@@ -5,73 +5,46 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.io.File;
 import java.net.URL;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.function.Predicate;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import com.saxatus.aiml.api.parsing.AIML;
 
 class AIMLFileReaderTest
 {
 
-    private Map<String, String> map = new HashMap<>();
+    static final URL COMPLEX_AIML = AIMLFileReaderTest.class.getResource("/complexAIML.aiml");
 
-    @BeforeEach
-    void setup()
+    @Test
+    void readComplexAIMLFile() throws AIMLCreationException
     {
+
+        AIMLFileReader reader = new AIMLFileReader(new File(COMPLEX_AIML.getFile()));
+        Collection<AIML> aimlList = reader.provide();
+        assertElements(8, aimlList, aiml -> true);
+
+        assertElements(2, aimlList, aiml -> "that".equals(aiml.getThat()));
+        assertElements(6, aimlList, aiml -> aiml.getThat() == null);
+
+        assertElements(5, aimlList, aiml -> "topic".equals(aiml.getTopic()));
+        assertElements(3, aimlList, aiml -> aiml.getTopic() == null);
+
+        assertElements(0, aimlList, aiml -> aiml.getPattern() == null);
+        assertElements(0, aimlList, aiml -> aiml.getTemplate() == null);
+
+        assertElements(2, aimlList, aiml -> "PATTERN".equals(aiml.getPattern()));
+        assertElements(2, aimlList, aiml -> "THAT1".equals(aiml.getPattern()));
+        assertElements(1, aimlList, aiml -> "THAT1".equals(aiml.getPattern()) && aiml.getTopic() == null);
+        assertElements(1, aimlList, aiml -> "<BOT NAME=\"TEST\"/> 123".equals(aiml.getPattern()));
+
     }
 
-    // @Test
-    void readComplexAIMLFile()
+    private void assertElements(int count, Collection<AIML> aimlList, Predicate<AIML> predicate)
     {
-        URL fileURL = this.getClass()
-                        .getResource("/complexAIML.aiml");
-
-        try
-        {
-            AIMLFileReader reader = new AIMLFileReader(new File(fileURL.getFile()));
-            map.put("test", "test");
-            Collection<AIML> streambable = reader.provide();
-            assertEquals(8, streambable.stream()
-                            .count());
-            assertEquals(2, streambable.stream()
-                            .filter(aiml -> "that".equals(aiml.getThat()))
-                            .count());
-            assertEquals(5, streambable.stream()
-                            .filter(aiml -> "topic".equals(aiml.getTopic()))
-                            .count());
-            assertEquals(3, streambable.stream()
-                            .filter(aiml -> aiml.getTopic() == null)
-                            .count());
-            assertEquals(6, streambable.stream()
-                            .filter(aiml -> aiml.getThat() == null)
-                            .count());
-            assertEquals(0, streambable.stream()
-                            .filter(aiml -> aiml.getPattern() == null)
-                            .count());
-            assertEquals(0, streambable.stream()
-                            .filter(aiml -> aiml.getTemplate() == null)
-                            .count());
-            assertEquals(2, streambable.stream()
-                            .filter(aiml -> "PATTERN".equals(aiml.getPattern()))
-                            .count());
-            assertEquals(2, streambable.stream()
-                            .filter(aiml -> "THAT1".equals(aiml.getPattern()))
-                            .count());
-            assertEquals(1, streambable.stream()
-                            .filter(aiml -> "THAT1".equals(aiml.getPattern()))
-                            .filter(aiml -> aiml.getTopic() == null)
-                            .count());
-            assertEquals(1, streambable.stream()
-                            .filter(aiml -> "TEST 123".equals(aiml.getPattern()))
-                            .count());
-
-        }
-        catch(Exception e)
-        {
-            e.printStackTrace();
-        }
+        assertEquals(count, aimlList.stream()
+                        .filter(predicate)
+                        .count());
     }
 
 }
